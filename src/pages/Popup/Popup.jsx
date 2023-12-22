9c9f43acad1bf931ce80c3219e534939ee1ad5d0
sanjuth <sanjuthreddy2003@gmail.com>
2023-12-22 21:33:03 +0530

    bootstrap

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,25 +1,90 @@
 import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
+import { useState, useEffect } from 'react';
 
 const Popup = () => {
+  const [username, setusername] = useState('');
+  const [message, setmessage] = useState('Fetching from leetcode ....');
+
+
+  useEffect(() => {
+    retrieveTokens();
+
+  }, [])
+  
+  const retrieveTokens = () => {
+    chrome.runtime.sendMessage({ action: 'fetchCookies' });
+  };
+
+  chrome.runtime.onMessage.addListener(function (
+    request,
+    sender,
+    sendResponse
+  ) {
+    if (request.action === 'cookiesFetched') {
+      const { csrfToken, leetcodeSession } = request;
+      console.log('Received cookies:', csrfToken, leetcodeSession);
+
+      if (csrfToken && leetcodeSession) {
+        var myHeaders = new Headers();
+        myHeaders.append('Content-Type', 'application/json');
+
+        var data = JSON.stringify({
+          csrfToken: csrfToken,
+          session: leetcodeSession,
+        });
+
+        var requestOptions = {
+          method: 'POST',
+          headers: myHeaders,
+          body: data,
+          redirect: 'follow',
+        };
+
+        fetch(
+          'https://084e-2405-201-c033-80be-ed2d-ea6a-26f1-62ed.ngrok-free.app/addUser',
+          requestOptions
+        )
+          .then((response) => response.text())
+          .then((result) => {
+            console.log(result);
+            const res = JSON.parse(result);
+            console.log('res', res);
+            if (res.username) {
+              setusername(res.username);
+            }
+          })
+          .catch((error) => console.log('error', error));
+      } else {
+        setmessage('Please Login at www.leetcode.com');
+      }
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+      <div className="title">
+        <p className="coding">WE</p>
+        <p className="buddies">CODES</p>
+      </div>
+      {/* <button onClick={retrieveTokens}>Get tokens</button> */}
+      {username !== '' ? (
+        <div>
+          <button>
+            <a
+              href={`http://192.168.29.112:5173/${username}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Join Room
+            </a>
+          </button>
+        </div>
+      ) : (
+        <div>
+          <p>{message}</p>
+        </div>
+      )}
     </div>
   );
 };
